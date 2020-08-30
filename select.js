482c2d08d87c4fdebe0f5a659ddd417f9868efb7
Ben Parker <bparker@gmail.com>
2020-08-30 13:57:19 -0500

    Updated placeholder text

--- select.js
+++ select.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
   $(".js-select2").select2({
     closeOnSelect: true,
-    placeholder: "Placeholder",
+    placeholder: "Where do you live?",
     // allowHtml: true,
     allowClear: true,
     tags: true, //
