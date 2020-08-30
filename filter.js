5a2fbec13d2358a9e0194c5ce83e53d80e367c19
Ben Parker <bparker@gmail.com>
2020-08-30 17:56:44 -0500

    Added filter js

---
+++ filter.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+  $("#states").on("keyup", function () {
+    let value = $(this).val().toLowerCase();
+    $(".location-name").filter(function () {
+      $(this)
+        .parent()
+        .toggle($(this).text().toLowerCase().indexOf(value) > -1);
+    });
+  });
+});
+$(document).on("keyup", "#search", function (e) {
+  if (e.keyCode == 13) {
+    $("input").blur();
+    Foundation.libs.topbar.toggle();
+  }
+});
