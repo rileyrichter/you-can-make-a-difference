9d2bc64dc01cdb35933b7f8167ce36b32be73738
Ben Parker <bparker@gmail.com>
2020-08-30 23:26:23 -0500

    Remove bottom border

--- filter.js
+++ filter.js
@@ -10,6 +10,7 @@ $(".button").click(function () {
 if ("mylocation" in localStorage) {
   let currentLocation = localStorage.getItem("mylocation");
   $(".yourlocation").append(currentLocation);
+  $(".yourlocation").child().css("border-bottom", "none");
   $(".yourlocation").closest(".w-embed").find("a").text("New Text");
 } else {
   $(".yourlocation").hide();
