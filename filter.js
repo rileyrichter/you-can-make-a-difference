2f6f2e3c843680f235d7f947f354e970fc31a560
Ben Parker <bparker@gmail.com>
2020-08-30 19:44:51 -0500

    Updated filter

--- filter.js
+++ filter.js
@@ -1,24 +1,3 @@
-$(document).ready(function () {
-  $("#search").on("keyup", function () {
-    let value = $(this).val().toLowerCase();
-    $(".location-name").filter(function () {
-      $(this)
-        .parent()
-        .toggle($(this).text().toLowerCase().indexOf(value) > -1);
-    });
-  });
-});
-$(document).on("keyup", "#search", function (e) {
-  if (e.keyCode == 13) {
-    $("input").blur();
-    Foundation.libs.topbar.toggle();
-  }
-});
 $("#clear").click(function () {
   document.getElementById("search").value = "";
-  $(".location-name").filter(function () {
-    $("search")
-      .parent()
-      .toggle($("search").text().toLowerCase().indexOf(value) > -1);
-  });
 });
