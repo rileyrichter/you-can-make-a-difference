be71aebde811cabef3fb70f4ee4082c864b4a5c3
Ben Parker <bparker@gmail.com>
2020-08-31 09:10:24 -0500

    removed files we didn't need

--- select.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-  $(".js-select2").select2({
-    closeOnSelect: true,
-    placeholder: `Where do you live?`,
-    // allowHtml: true,
-    allowClear: false,
-    tags: true, //
-  });
-});
