25326234d0486d3e5dfdea045fa57dcd41f9a516
Ben Parker <bparker@gmail.com>
2020-09-02 11:32:53 -0500

    Changed the names of a js and html file, added notes to js

---
+++ location.js
@@ -0,0 +1,41 @@
+// Add a location as a favorite in localStorage
+// When you click a button with the class of .live-button
+$(".live-button").click(function () {
+  // Let myLocation = the html of the .location-grid element
+  let myLocation = $(this).parent().parent().parent().parent().html();
+  // Set that html as the value in local storage with the key of mylocation
+  localStorage.setItem("mylocation", myLocation);
+});
+
+// Add the ability to remove a location
+// When you click the button with an ID of remove
+$("#remove").click(function () {
+  // remove the mylocation item from local storage
+  localStorage.removeItem("mylocation");
+  // hide the div with a class of .yourlocation
+  $(".yourlocation").hide();
+  // hide the div with a class of .current-location-wrapper
+  $(".current-location-wrapper").hide();
+});
+
+// If else statement, this checks whether there is a mylocation in localStorage
+$(document).ready(function () {
+  // If there is a mylocation in localStorage
+  if ("mylocation" in localStorage) {
+    // set a variable called currentLocation and make the value the mylocation localStorage item
+    let currentLocation = localStorage.getItem("mylocation");
+    // append that html in local storage to the .yourlocation div
+    $(".yourlocation").append(currentLocation);
+    // style .yourlocation by removing the border and changing the background color
+    $(".yourlocation").children().css("border-bottom", "none");
+    $(".yourlocation").children().css("background-color", "transparent");
+    // change the text of the button so it's different that when it's in the list
+    $(".yourlocation").find("a").text("Contact your reps!");
+    // make sure to show the .current-location-wrapper
+    $(".current-location-wrapper").show();
+  } else {
+    // if there's nothing in localStorage, hide .yourlocation and .current-location-wrapper
+    $(".yourlocation").hide();
+    $(".current-location-wrapper").hide();
+  }
+});
